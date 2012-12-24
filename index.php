aded87dc75cc4bd45ad904ee806a2b4681919181
Colin Waddell <mrcolin@gmail.com>
2012-12-24 19:55:11 +0000

    Added analytics
    Need to create new ID analytics ID for this at some point

--- index.php
+++ index.php
@@ -131,6 +131,15 @@
 				</footer>
 			</div>
 		</div> <!-- wrapper -->
+		
+		
+		<script>
+			var _gaq=[['_setAccount','UA-13023652-3'],['_trackPageview']];
+			(function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
+			g.src=('https:'==location.protocol?'//ssl':'//www')+'.google-analytics.com/ga.js';
+			s.parentNode.insertBefore(g,s)}(document,'script'));
+		</script>
+		
 	</body>
 	
 </html>
